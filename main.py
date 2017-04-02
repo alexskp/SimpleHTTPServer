80b950f886005695d239cc25ac2c90ea64a9bd43
Alexander Skopets <skopets.sasha@gmail.com>
2017-04-03 02:02:30 +0300

    finding and opening index.html/index.htm

--- main.py
+++ main.py
@@ -40,35 +40,45 @@ def check_client(conn, addr):
 	while not b"\r\n\r\n" in data:
 		recieve = conn.recv(1024)
 		data += recieve
-		
+
 	utf_data = data.decode("utf-8")
 
 	method, address, params = utf_data.split(" ", 2)
 	protocol, params = params.split("\r\n", 1)
-	
+
 	conn_time = time.strftime("[%d/%b/%G %H:%M:%S]")
 	print("%s - - %s \"%s %s %s\" 200 -" % (addr, conn_time ,method, address, protocol))
-	
-	
-	
-	#lst = os.listdir(address)
-	
-	#if 'index.html' not in lst:
-	#	send_answer(conn, "404 Not Found", data="404 Not Found")
-	#else: 
-	#	conn.send(b"HTTP/1.1 200 OK")
-	#	send_dir_list()
-	
+	#print(params)
 
+	check_path(conn, address)
+	conn.close()
 
-	
+def check_path(conn, path):
+	work_dir = os.getcwd()
+	#print(work_dir + path)
 
+	if os.path.isdir(work_dir + path):
+		#print(work_dir + path + "CONTACT")
+		for index in "/index.html", "/index.htm":
+			if os.path.isfile('.' + path + index):
+				f = open(os.path.normpath('.' + path + index))
+				send_answer(conn, typ="text/html", data=f.read())
+			else:
+				send_answer(conn)
+			break
+
+	elif os.path.isfile('.' + path):
+		f = open('.' + path)
+		send_answer(conn, typ="text/html", data=f.read())
+	else:
+		send_answer(conn, "404 Not Found", data="Не найдено")
 
 
 
 #def index_read():
 #	f = open("index.html")
-
+#send_answer(conn, status="302 Moved Temporarily", loc="localhost:8000/index.html"
+#conn.send(b"Location: " + loc.encode("utf-8")+ b"\r\n")
 ##########################################################
 port = get_port()
 
@@ -83,21 +93,13 @@ try:
 		client_soc, client_addr = serv_soc.accept()
 		try:
 			check_client(client_soc, client_addr[0])
-			f = open("index.html")
-			send_answer(client_soc, data=f.read())
+
 		except:
-			send_answer(client_soc, "500 Internal Server Error", data="500 Internal Server Error")
+			send_answer(client_soc, status="500", typ="500 Internal Server Error", data="500 Internal Server Error")
+			raise
 		finally:
-			client_soc.close()	
+			client_soc.close()
 except KeyboardInterrupt:
 	print("KeyboardInterrupt")
 finally:
 	serv_soc.close()
-
-
-
-
-
-
-
-
